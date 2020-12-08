72f2d51e3c5015ad960fc479497ca17948092a2e
mwang20 <mwang20@stuy.edu>
2020-12-08 08:39:07 -0500

    Finished length method.

--- Range.java
+++ Range.java
@@ -2,8 +2,8 @@ import java.util.NoSuchElementException;
 import java.util.ArrayList;
 public class Range implements IntegerSequence{
   private int start,end,current;
+  ArrayList<Integer> rList = new ArrayList();
   public Range(int start,  int end){
-    ArrayList<Integer> rList = new ArrayList();
     for (int i = 0; i < (end - start); i++){
       current = i + start;
       rList.add(i + end);
@@ -11,5 +11,8 @@ public class Range implements IntegerSequence{
   }
   public void reset(){
     current = start;
+  }
+  public int length(){
+    return rList.size();
   }
 }
