120d45be7a1eb8416e7868869be2d6e858557d16
mwang20 <mwang20@stuy.edu>
2020-12-08 12:51:37 -0500

    Created the ArraySequence consttructor using for loop making every index of data equal the value of the corresponding index in other array.

--- ArraySequence.java
+++ ArraySequence.java
@@ -2,4 +2,10 @@ import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
   private int currentIndex;
   private int []data;
+  public ArraySequence(int [] other){
+    data = new int[other.length];
+    for (int i = 0; i < other.length; i++){
+      data[i] = other[i];
+    }
+  }
 }
